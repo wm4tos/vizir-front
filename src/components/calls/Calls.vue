336e0a7c0f84ba5eeea8b88d832efe1ab5fa0c6c
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-22 15:10:52 -0300

    apagar destino

--- src/components/calls/Calls.vue
+++ src/components/calls/Calls.vue
@@ -21,28 +21,36 @@
           />
         </q-input>
       </div>
-      <div class="row gutter-y-md col-12">
+      <div class="row justify-center gutter-y-md col-12">
         <div class="col-12">
           <h5 class="text-center no-margin"> Destinos </h5>
         </div>
         <div
-          class="row justify-center gutter-xs col-12"
-          v-for="item in destinys"
+          class="row justify-between items-center gutter-xs col-12"
+          v-for="(item, index) in destinys"
           :key="item.id"
         >
-          <div class="col-xs-12 col-sm-6">
+          <div class="col-xs-12 col-sm-5">
             <q-input
               v-model="item.destiny"
               float-label="DDD do destino"
               type="tel"
               v-mask="'##'"/>
           </div>
-          <div class="col-xs-12 col-sm-6">
+          <div class="col-xs-12 col-sm-5">
             <q-input
               v-model="item.price"
               v-money="money"
               float-label="Valor"
               type="tel"/>
+          </div>
+          <div class="col-xs-12 col-sm-1 flex justify-end ">
+            <q-btn
+              icon="delete"
+              round
+              flat
+              @click="destinys.splice(index, 1)"
+              :disabled="destinys.length <= 1"/>
           </div>
         </div>
       </div>
