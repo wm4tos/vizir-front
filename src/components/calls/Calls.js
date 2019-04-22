cfad979a17ca36c13526b0b5df679b08bd141bf1
Wesley Matos <wrickee@gmail.com>
2019-04-22 00:12:58 -0300

    chamar função "ResetData" depois de salvar ou editar ligação

--- src/components/calls/Calls.js
+++ src/components/calls/Calls.js
@@ -33,7 +33,7 @@ export default {
         } else {
           await this.$axios.post('call', req);
         }
-        this.$emit('input', false);
+        this.ResetData(false);
       } catch (err) {
         throw new Error(err);
       }
